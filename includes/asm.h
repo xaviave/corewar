5f5f9a29fb3c8cdaaf41f10d80d38c8e8a4a3966
Xavier-emmanuel Moreau <xmoreau@z4r3p4.le-101.fr>
2018-06-20 18:16:14 +0200

    test de xem

--- includes/asm.h
+++ includes/asm.h
@@ -6,8 +6,15 @@
 /*   By: xmoreau <xmoreau@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/06/20 17:53:02 by xmoreau      #+#   ##    ##    #+#       */
-/*   Updated: 2018/06/20 18:00:57 by xmoreau     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/06/20 18:03:59 by xmoreau     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
+#ifndef ASM_H
+# define ASM_H
+
+# include "../libft/libft.h"
+# include "../op/op.h"
+
+#endif
--- scrs_asm/main_asm.c
+++ scrs_asm/main_asm.c
@@ -3,17 +3,27 @@
 /*                                                              /             */
 /*   main_asm.c                                       .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
-/*   By: xamartin <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*   By: xmoreau <xmoreau@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/06/19 13:49:01 by xamartin     #+#   ##    ##    #+#       */
-/*   Updated: 2018/06/19 13:49:33 by xamartin    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/06/20 18:10:48 by xmoreau     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-#include "../includes/corewar.h"
+#include "../includes/asm.h"
 
-int main(int ac, char **av)
+int 	main(int ac, char **av)
 {
+	if (ac < 2)
+		ft_printf("Usage: ./src_asm [-a] <sourcefile.s>\n\t-a : 
+		Instead of creating a .cor file, outputs a stripped and 
+		annotated version of the code to the standard output");
+	else
+		red_code(av);
 	return (0);
 }
+/*
+si le fichier n'exite pas : Can't read source file "file_name"
+si pas de ".s" le fichier .cor n'a pas de nom et s'appelle ".cor"
+*/
