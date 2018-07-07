f577b34d213a191e35ab5466ca7868fecd30e2b3
ltoussai <louistoussaint98@gmail.com>
2018-07-07 04:10:14 +0200

    sommeil, modif fichier third

--- srcs_asm/ft_third.c
+++ srcs_asm/ft_third.c
@@ -1,30 +1,42 @@
 #include "../includes/asm.h"
 #include <fcntl.h>
 
+void			ft_errase(int *fd, char **name)
+{
+	*fd = open(*name, O_RDWR);
+	lseek(*fd, 0, SEEK_SET);
+	write(*fd, "a", 1);
+}
+
+static void		ft_exit_third(char **name, int *fd)
+{
+	free(*name);
+	close(*fd);
+	exit(1);
+}
+
 void			ft_name_file(int argc, char **argv, int *fd)
 {
 	char	*name;
 	char	*tmp;
 
-	if (!(name = ft_strndup(argv[argc - 1], ft_strlen(argv[argc - 1]))))
+	if (!(name = ft_strdup(argv[argc - 1])))
 		exit(1);
-	if (name[ft_strlen(name) - 2] == '.' && name[ft_strlen(name) - 1] == 's')
-		{
-			tmp = name;
-			name = ft_strjoin(ft_strndup(name, ft_strlen(name) - 2), ".cor");
-			free(tmp);
-			if ((*fd = open(name, O_CREAT) == -1))
-				exit(1);
-		}
-	else
+	if (!ft_strcmp((name + ft_strlen(name) - 2), ".s"))
 	{
-		if ((*fd = open(".cor", O_CREAT) == -1))
-		{
-			free(name);
-			exit(1);
-		}
+		name = realloc(name, ft_strlen(name) - 2);
+		tmp = name;
+		ft_printf("%s\n", name);
+		name = ft_strjoin(name, ".cor");
+		free(tmp);
+		ft_printf("%s\n", name);
+		if ((*fd = open(name, O_RDWR | O_CREAT, S_IRUSR + S_IWUSR) == -1))
+			ft_exit_third(&name, fd);
 	}
-	free(name);
+	else
+		if ((*fd = open(".cor", O_RDWR | O_CREAT, S_IRUSR + S_IWUSR) == -1))
+			ft_exit_third(&name, fd);
+	ft_printf("name = %s\n/", name);
 }
 
 int				ft_third(char **argv, int argc)
