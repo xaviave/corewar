6a542d22b182ef07325d31a0cb71f1d7c296c5b6
ltoussai <louistoussaint98@gmail.com>
2018-07-09 21:38:07 +0200

    finir lexer

--- srcs_asm/ft_parsing.c
+++ srcs_asm/ft_parsing.c
@@ -2,7 +2,8 @@
 
 int			ft_char(char let)
 {
-	if (let == '.' || let == ' ' || let == '"' || let == '\n' || let == ',')
+	if (let == '.' || let == ' ' || let == '"' || let == '\n' || let == ','
+		|| let == '\t')
 		return (0);
 	else
 		return (1);
@@ -16,21 +17,33 @@ t_list		*ft_lexer(char *file)
 	t_list	*list;
 	t_list	*tmp;
 
-	if (!(list = ft_lstnew(list, sizeof(t_list))))
-		return (NULL);
 	glu[1] = '\0';
+	list = NULL;
 	while (*file)
 	{
 		tkn = ft_strdup("token=");
 		while (ft_char(*file))
 		{
-			glu[0] = *file;
+			glu[0] = *file++;
 			fre = tkn;
 			tkn = ft_strjoin(tkn, glu);
 			free(fre);
 		}
-		/*liste*/
+		if (!ft_strcmp(tkn, "token="))
+		{
+			fre = tkn;
+			glu[0] = *file++;
+			tkn = ft_strjoin(tkn, glu);
+			free(fre);
+		}
+		if (!(tmp = ft_lstnew(tkn, sizeof(ft_strlen(tkn)))))
+			return (NULL);
+		ft_printf("list = %s\n", tkn);
+		ft_lstadd(&list, tmp);
+		free(tmp);
+		free(tkn);
 	}
+	return (list);
 }
 
 int			ft_parsing(char *file)
@@ -42,4 +55,5 @@ int			ft_parsing(char *file)
 		free(file);
 		exit(1);
 	}
+	return (0);
 }
