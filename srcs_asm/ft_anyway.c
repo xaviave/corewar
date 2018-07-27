0964a81a42620b9dc067e991184bcb2fab10fc4d
ltoussai <louistoussaint98@gmail.com>
2018-07-27 18:04:45 +0200

    le code cest cancer

--- srcs_asm/ft_anyway.c
+++ srcs_asm/ft_anyway.c
@@ -1,8 +1,53 @@
 #include "../includes/asm.h"
 
+int			ft_size_base(char **tkn, int j)
+{
+	int		i;
+	int		plus_size;
+
+	i = 0;
+	plus_size = 0;
+	while (tkn[i])
+	{
+		if (i > j)
+			if (!ft_strcmp(tkn[i], "#"))
+				plus_size += 1;
+		i++;
+	}
+	return (i + plus_size);
+}
+
+int			ft_recreate_base(t_all *a, int j)
+{
+	char	**rec;
+	int		i;
+	int		k;
+
+	i = 0;
+	k = 0;
+	if (!(rec = ft_memalloc(ft_size_base(a->base.tkn, j))))
+		return (0);
+	while (a->base.tkn[i])
+	{
+		if (!ft_strcmp(a->base.tkn[i], "#") && i > j)
+			if (!(rec[k++] = ft_strdup(" ")))
+				return (0);
+		if (!(rec[k++] = ft_strdup(a->base.tkn[i++])))
+			return (0);
+	}
+	rec[k] = NULL;
+	i = 0;
+	while (a->base.tkn[i])
+		ft_strdel(&a->base.tkn[i++]);
+	free(a->base.tkn);
+	a->base.tkn = rec;
+	return (1);
+}
+
 int			ft_anyway(t_all *a)
 {
 	int		i;
+	int		j;
 	char	*tmp;
 
 	i = 0;
@@ -13,15 +58,17 @@ int			ft_anyway(t_all *a)
 			i++;
 		i++;
 	}
+	j = i;
 	while (a->base.tkn[i])
 	{
 		if (!ft_strcmp(a->base.tkn[i], ";"))
 		{
 			tmp = a->base.tkn[i];
-			a->base.tkn[i] = ft_strdup("#");
-			free(tmp);
+			if (!(a->base.tkn[i] = ft_strdup("#")))
+				return (0);
+			ft_strdel(&tmp);
 		}
 		i++;
 	}
-	return (1);
+	return (ft_recreate_base(a, j));
 }
--- srcs_asm/zork.s
+++ srcs_asm/zork.s
@@ -1,8 +1,10 @@
 .name "zo;rk"
 .comment "I'M ALIIIIVE"
-live %0#  ntm
-l2:		sti r1, %:live, %1
+live %0
+l2:		sti r1, %:live, %1 ##;
 		and r1, %0, r1
-
+		live %0 ; #
+;
 live:	live %1
-		zjmp %:live
+		zjmp %:live ;
+ ;
