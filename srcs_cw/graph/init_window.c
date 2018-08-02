6790a3fd29f917c26e890e96cdd1788fc8f8b5c1
tristan <tristan@tristans-Mac.local>
2018-08-02 07:54:22 -0700

    adieu menu

--- srcs_cw/graph/init_window.c
+++ srcs_cw/graph/init_window.c
@@ -13,91 +13,7 @@
 
 #include "../../includes/corewar.h"
 
-char *choices[] =
-{
-	"Speed x1000",
-	"Speed x500",
-	"Speed x50",
-	"Speed x1",
-};
-
-void print_menu(WINDOW *menu_win, int highlight)
-{
-	int x;
-	int y;
-	int i;
-
-	i = -1;
-	x = 2;
-	y = 2;
-	box(menu_win, 0, 0);
-	while (++i < (int)(sizeof(choices) / sizeof(char *)))
-	{
-		if (highlight == i + 1)
-		{
-			wattron(menu_win, A_REVERSE); 
-			mvwprintw(menu_win, y, x, "%s", choices[i]);
-			wattroff(menu_win, A_REVERSE);
-		}
-		else
-			mvwprintw(menu_win, y, x, "%s", choices[i]);
-		++y;
-	}
-	move(0, COLS);
-	wrefresh(menu_win);
-}
-
-void init_menu(t_mem *mem)
-{
-	WINDOW *menu_win;
-	int highlight;
-	int choice;
-	int c;
-
-	choice = 0;
-	highlight = 1;
-	initscr();
-	clear();
-	noecho();
-	cbreak();
-	menu_win = newwin(8, 20, 20, 40);
-	keypad(menu_win, TRUE);
-	mvprintw(0, 0, "Use arrow keys to go up and down, Press enter to select a choice");
-	refresh();
-	print_menu(menu_win, highlight);
-	while(1)
-	{c = wgetch(menu_win);
-		switch(c)
-		{case KEY_UP:
-			if(highlight == 1)
-				highlight = sizeof(choices) / sizeof(char *);
-			else
-				--highlight;
-			break;
-			case KEY_DOWN:
-			if(highlight == sizeof(choices) / sizeof(char *))
-				highlight = 1;
-			else 
-				++highlight;
-			break;
-			case 10:
-			choice = highlight;
-			break;
-			default:
-			refresh();
-			break;
-		}
-		print_menu(menu_win, highlight);
-		if(choice != 0)
-			break;
-	}
-	mem->speed = (117 * ft_pow(choice, 3) - 652 * ft_pow(choice, 2) +
-													137 * choice + 2398) / 2;
-	refresh();
-	free(menu_win);
-}
-
-void		print_command()
+static void			print_command()
 {
 	attron(COLOR_PAIR(5));
 	move(52, 195);
@@ -132,13 +48,13 @@ void	init_window(t_mem *mem, t_champ *list, t_arg *args, int *stop)
 	noecho();
 	cbreak(); 
 	start_color();
+	mem->speed = 50;
 	init_pair(1, COLOR_GREEN, COLOR_BLACK);
 	init_pair(2, COLOR_BLUE, COLOR_BLACK);
 	init_pair(3, COLOR_RED, COLOR_BLACK);
 	init_pair(4, COLOR_CYAN, COLOR_BLACK);
 	init_pair(5, COLOR_WHITE, COLOR_BLACK);
 	init_pair(6, COLOR_YELLOW, COLOR_BLACK);
-	init_menu(mem);
 	nodelay(stdscr, 1);
 	print_command();
 	*stop = print_mem(mem, list, args, 1);
