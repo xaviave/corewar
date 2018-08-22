85134fcc73a1cfbdb3821a8cb5349d967e5604cf
xaviave <hellxaviave@gmail.com>
2018-08-22 16:21:25 +0200

    norme

--- includes/corewar.h
+++ includes/corewar.h
@@ -6,7 +6,7 @@
 /*   By: xamartin <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/06/19 13:50:45 by xamartin     #+#   ##    ##    #+#       */
-/*   Updated: 2018/07/24 20:58:52 by tduverge    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/08/22 14:56:59 by tduverge    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -20,8 +20,8 @@
 # include <ncurses.h>
 
 /*
- * STRUCTURES
- */
+** STRUCTURES
+*/
 
 typedef struct		s_champ
 {
@@ -69,8 +69,8 @@ typedef struct		s_mem
 }					t_mem;
 
 /*
- * INITIALISATION
- */
+** INITIALISATION
+*/
 
 int					all_init_is_love(t_champ **l, t_arg *ar, int ac, char **av);
 int					init_champ(t_champ **champ, t_arg *args);
@@ -78,8 +78,8 @@ void				generate_memory(t_champ **list, t_mem *aff, t_arg *args);
 int					parse_arg(int ac, char **av, t_arg *args);
 
 /*
- * FONCTIONS PRINCIPALES
- */
+** FONCTIONS PRINCIPALE
+*/
 
 int					lets_go(t_champ **l, t_mem *m, t_arg *a);
 int					lets_dump(t_champ **l, t_mem *m, t_arg *a);
@@ -87,29 +87,29 @@ int					lets_graph(t_champ **list, t_mem *mem, t_arg *args);
 void				one_cycle(t_champ **list, t_mem *mem, t_arg *args, int *v);
 
 /*
- * FONCTIONS UPCODE
- */
+** FONCTIONS UPCODE
+*/
 
-int		ft_live(t_champ *tmp, t_champ **list, t_mem *mem, t_arg *arg);
-int		ft_ld(t_champ *tmp, t_champ **list, t_mem *mem, t_arg *arg);
-int		ft_st(t_champ *tmp, t_champ **list, t_mem *mem, t_arg *arg);
-int		ft_add(t_champ *tmp, t_champ **list, t_mem *mem, t_arg *arg);
-int		ft_sub(t_champ *tmp, t_champ **list, t_mem *mem, t_arg *arg);
-int		ft_and(t_champ *tmp, t_champ **list, t_mem *mem, t_arg *arg);
-int		ft_or(t_champ *tmp, t_champ **list, t_mem *mem, t_arg *arg);
-int		ft_xor(t_champ *tmp, t_champ **list, t_mem *mem, t_arg *arg);
-int		ft_zjmp(t_champ *tmp, t_champ **list, t_mem *mem, t_arg *arg);
-int		ft_ldi(t_champ *tmp, t_champ **list, t_mem *mem, t_arg *arg);
-int		ft_sti(t_champ *tmp, t_champ **list, t_mem *mem, t_arg *arg);
-int		ft_fork(t_champ *tmp, t_champ **list, t_mem *mem, t_arg *arg);
-int		ft_lld(t_champ *tmp, t_champ **list, t_mem *mem, t_arg *arg);
-int		ft_lldi(t_champ *tmp, t_champ **list, t_mem *mem, t_arg *arg);
-int		ft_lfork(t_champ *tmp, t_champ **list, t_mem *mem, t_arg *arg);
-int		ft_aff(t_champ *tmp, t_champ **list, t_mem *mem, t_arg *arg);
+int					ft_live(t_champ *tmp, t_champ **list, t_mem *mem, t_arg *a);
+int					ft_ld(t_champ *tmp, t_champ **list, t_mem *mem, t_arg *arg);
+int					ft_st(t_champ *tmp, t_champ **list, t_mem *mem, t_arg *arg);
+int					ft_add(t_champ *tmp, t_champ **list, t_mem *mem, t_arg *ar);
+int					ft_sub(t_champ *tmp, t_champ **list, t_mem *mem, t_arg *ar);
+int					ft_and(t_champ *tmp, t_champ **list, t_mem *mem, t_arg *ar);
+int					ft_or(t_champ *tmp, t_champ **list, t_mem *mem, t_arg *arg);
+int					ft_xor(t_champ *tmp, t_champ **list, t_mem *mem, t_arg *ar);
+int					ft_zjmp(t_champ *tmp, t_champ **list, t_mem *mem, t_arg *a);
+int					ft_ldi(t_champ *tmp, t_champ **list, t_mem *mem, t_arg *ar);
+int					ft_sti(t_champ *tmp, t_champ **list, t_mem *mem, t_arg *ar);
+int					ft_fork(t_champ *tmp, t_champ **list, t_mem *mem, t_arg *a);
+int					ft_lld(t_champ *tmp, t_champ **list, t_mem *mem, t_arg *ar);
+int					ft_lldi(t_champ *tmp, t_champ **list, t_mem *mem, t_arg *a);
+int					ft_lfork(t_champ *tmp, t_champ **list, t_mem *me, t_arg *a);
+int					ft_aff(t_champ *tmp, t_champ **list, t_mem *mem, t_arg *ar);
 
 /*
- * FONCTIONS GRAPHIQUES
- */
+** FONCTIONS GRAPHIQUES
+*/
 
 void				init_window(t_mem *mem, t_champ *list, t_arg *args, int *s);
 int					print_mem(t_mem *mem, t_champ *list, t_arg *args, int stop);
@@ -118,15 +118,19 @@ void				legend(t_champ *list, t_mem *mem, t_arg *args, int col);
 void				info_cycle(t_champ *list, t_mem *mem);
 
 /*
- * OUTILS DIVERS
- */
+** OUTILS DIVERS
+*/
 
-/*  Lecture et ecriture des registres */
+/*
+ ** Lecture et ecriture des registres
+*/
 
 void				write_reg(t_champ *champ, int nb_of_reg, int nb_to_write);
 int					give_reg(t_champ *champ, int number);
 
-/*  Recuperation des donnees */
+/*
+** Recuperation des donnees
+*/
 
 unsigned char		recup_octet(t_mem *mem, t_champ *champ, int n);
 unsigned int		recup_direct2(t_mem *mem, t_champ *champ, int start);
@@ -136,13 +140,17 @@ unsigned int		recup_indirect4(t_mem *mem, t_champ *champ, int start);
 unsigned int		recup_indirect2x(t_mem *mem, t_champ *champ, int start);
 unsigned int		recup_indirect4x(t_mem *mem, t_champ *champ, int start);
 
-/*  Mouvements du pc, y compris en cas d'erreurs */
+/*
+** Mouvements du pc, y compris en cas d'erreurs
+*/
 
 int					mod_pc(t_champ *tmp, int nu);
 int					ft_error2(char code, t_champ *tmp, int nu);
 int					ft_error4(char code, t_champ *tmp, int nu);
 
-/*  Divers */
+/*
+** Divers
+*/
 
 int					list_len(t_champ *list);
 int					check_cor(int ac, char **av);
@@ -151,8 +159,8 @@ int					check_cor_nb(int ac, char **av);
 void				kill_them_all(t_champ **list);
 
 /*
- * **   KEY
- * */
+** KEY
+*/
 
 # define KEY_1 49
 # define KEY_2 50
